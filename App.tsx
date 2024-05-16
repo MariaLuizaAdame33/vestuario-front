c3af7d1ca29fa3b75b32153852307b37e1ff383f
MariaLuizaAdame33 <maluzinha.adame@gmail.com>
2024-05-16 15:44:03 -0300

    first commit

---
+++ App.tsx
@@ -0,0 +1,22 @@
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import CadastroScreen from './src/screens/CadastroScreen';
+
+
+const Stack = createStackNavigator();
+
+
+function App(): React.ReactElement {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name='Cadastro' component={CadastroScreen} options={{ headerShown: false }} />
+      </Stack.Navigator>
+    </NavigationContainer>
+
+  );
+}
+
+
+
+export default App;
